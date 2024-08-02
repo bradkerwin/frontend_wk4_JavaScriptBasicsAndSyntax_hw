6074dedb5cd4d7419d2be0efa8e883b9de833ab9
bradkerwin <bkerwin101@gmail.com>
2024-08-02 18:55:10 -0400

    initial commit

---
+++ ExploringJavaScriptOperators.js
@@ -0,0 +1,19 @@
+let num1 = 5
+let num2 = 3
+
+console.log("Sum:", (num1, num2));
+console.log("Difference:", (num1, num2));
+console.log("Product:", (num1, num2));
+console.log("Quotient:", (num1, num2));
+
+console.log("Updated num1:", 9);
+console.log("Updated num2:", 6);
+
+console.log("Is num1 equal to num2", num1 == num2);
+console.log("Is num1 > num2", num1 > num2);
+console.log("Is num1 not equal to num2", num1 != num2);
+
+let isPositive = 6
+let isEven = 4
+console.log("Are both numbers positive?", isPositive);
+console.log("Is one or both of the numbers even?", isEven);
---
+++ UnderstandingDataTypesinJavaScript.js
@@ -0,0 +1,17 @@
+// Task 1:
+let name = "Name";
+let age = "age";
+let grade = "grade";
+let attendanceStatus = "attendanceStatus";
+
+// Task 2:
+name = "Tom";
+age = "16";
+grade = "10th";
+attendanceStatus = "present";
+
+// Task 3:
+console.log(name);
+console.log(age);
+console.log(grade);
+console.log(attendanceStatus);
