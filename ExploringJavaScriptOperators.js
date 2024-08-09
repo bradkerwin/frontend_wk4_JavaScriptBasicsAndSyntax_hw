cef9885d4da4d05bb8d4a33f89e735dd297f7ee4
bradkerwin <bkerwin101@gmail.com>
2024-08-09 00:25:20 -0400

    modifications added to operations and conditionals

--- ExploringJavaScriptOperators.js
+++ ExploringJavaScriptOperators.js
@@ -1,19 +1,34 @@
-let num1 = 5
-let num2 = 3
+let num1 = 5;
+let num2 = 3;
 
-console.log("Sum:", (num1, num2));
-console.log("Difference:", (num1, num2));
-console.log("Product:", (num1, num2));
-console.log("Quotient:", (num1, num2));
+let sum = num1 + num2;
+let difference = num1 - num2;
+let product = num1 * num2;
+let quotient = num1 / num2;
 
-console.log("Updated num1:", 9);
-console.log("Updated num2:", 6);
+console.log("Sum:", sum);
+console.log("Difference:", difference);
+console.log("Product:", product);
+console.log("Quotient:", quotient);
 
-console.log("Is num1 equal to num2", num1 == num2);
-console.log("Is num1 > num2", num1 > num2);
-console.log("Is num1 not equal to num2", num1 != num2);
+let updatedNum = 9;
 
-let isPositive = 6
-let isEven = 4
-console.log("Are both numbers positive?", isPositive);
-console.log("Is one or both of the numbers even?", isEven);
+let newSum = sum + updatedNum;
+let newDifference = difference - updatedNum;
+let newProduct = product * updatedNum;
+let newQuotient = quotient / updatedNum;
+
+
+console.log("New Sum:", newSum);
+console.log("New Difference:", newDifference);
+console.log("New Product:", newProduct);
+console.log("New Quotient:", newQuotient);
+
+console.log("Is num1 equal to num2?", num1 == num2);
+console.log("Is num1 greater than num2?", num1 > num2);
+console.log("Is num1 less than num2?", num1 < num2);
+console.log("Is num1 not equal to num2?", num1 != num2);
+
+let isEqualto = num1 == quotient;
+
+console.log("Is num1 equal to the quotient variable?", isEqualto);
